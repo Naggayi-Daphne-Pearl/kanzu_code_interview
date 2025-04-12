09cc2f3b1eace917e1b98fcc546ca24e0af2011f
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2025-04-12 11:44:44 +0300

    Add Tailwind

--- SACCOApp/app/_layout.tsx
+++ SACCOApp/app/_layout.tsx
@@ -5,6 +5,8 @@ import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
+// Import the global.css file in the index.js file:
+import '../global.css';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
---
+++ SACCOApp/app/index.tsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <div className="bg-slate-100 rounded-xl">
+      <p className="text-lg font-medium">Welcome to Tailwind</p>
+    </div>
+  );
+}
